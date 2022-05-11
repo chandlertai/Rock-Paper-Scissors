21dd2f8de3c2d241796bd98c352c5373c918240d
Zack Barrell <zpbarrell@gmail.com>
2022-05-11 16:57:18 -0400

    using statements and repo.cs added

--- ConsoleGame/ConsoleGame.cs/Program.cs
+++ ConsoleGame/ConsoleGame.cs/Program.cs
@@ -1,6 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleGame;
 
-// TEST COMMENT
-//git sucks!
-//Fixed??
+Console.WriteLine("Choose Rock, Paper or Scissors");
+
+string userInput = Console.ReadLine();
+// this variable of userInput will store whatever option the user types into the console
