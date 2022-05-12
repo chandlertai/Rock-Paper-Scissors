fdeb87fb1f5db269f4b2a89f7d189a5949b68a33
David Sullivan <dcsulliv@gmail.com>
2022-05-11 21:28:24 -0400

    day 2

--- ConsoleGame/ConsoleGame.cs/Program.cs
+++ ConsoleGame/ConsoleGame.cs/Program.cs
@@ -1,6 +1,65 @@
 ﻿using ConsoleGame;
+Random optionsIndex = new Random();
+bool isRunning = true;
 
+// this variable of userInput will store whatever option the user types into the console
+
+string[] options = {"rock", "paper", "scissors"};
+
+string player = "";
+string computer = options[optionsIndex.Next(options.Length)];
+
+
+while (isRunning)
+{
 Console.WriteLine("Choose Rock, Paper or Scissors");
 
 string userInput = Console.ReadLine();
-// this variable of userInput will store whatever option the user types into the console
+
+switch (computer)
+{
+    case: "rock"
+    if (player == "paper")
+    {
+        Console.WriteLine("You Win!!!");
+    }
+    else if (player == "scissors")
+    {
+        Console.WriteLine("You Lose Bitch!!!!");
+    } else if (player == "rock")
+    {
+        Console.WriteLine("It's A Tie!!!");
+    }
+    break;
+
+    case: "paper"
+    if (player == "rock")
+    {
+        Console.WriteLine("You Lose!!!");
+    }
+    else if (player == "Scissors")
+    {
+        Console.WriteLine("You Win!!!");
+    }
+    else if (player == "paper")
+    {
+        Console.WriteLine("It's A Tie!!!");
+    }
+    break;
+
+    case: "scissors"
+    if (player == "rock")
+    {
+        Console.WriteLine("You Win!!!");
+    }
+    else if (player == "Paper")
+    {
+        Console.WriteLine("You Lose!!!");
+    }
+    else if (player == "scissors")
+    {
+        Console.WriteLine("It's a Tie!!!");
+    }
+}
+
+}
