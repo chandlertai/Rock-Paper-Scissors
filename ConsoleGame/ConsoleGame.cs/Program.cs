eb974f6e5995b332245d33336a346cef2ff69dea
Zack Barrell <zpbarrell@gmail.com>
2022-05-12 20:56:17 -0400

    checkpoint

--- ConsoleGame/ConsoleGame.cs/Program.cs
+++ ConsoleGame/ConsoleGame.cs/Program.cs
@@ -1,69 +1,27 @@
-﻿using ConsoleGame;
-Random optionsIndex = new Random();
-bool isRunning = true;
+﻿
+using ConsoleGame.cs;
 
-<<<<<<< HEAD
-Console.WriteLine("Choose Rock, Paper, or Scissors");
-=======
+Repository _rps = new Repository();
+
+Random optionsIndex = new Random();
+
+
+
 // this variable of userInput will store whatever option the user types into the console
 
 string[] options = {"rock", "paper", "scissors"};
 
-string player = "";
-string computer = options[optionsIndex.Next(options.Length)];
+_rps.RockPaperScissors();
+// string player = "";
+// _rps.options[optionsIndex.Next(options.Length)]();
+
+string computer = options[optionsIndex.Next()];
+// while (isRunning)
+// {
+// Console.WriteLine("Choose Rock, Paper or Scissors");
+
+// string userInput = Console.ReadLine();
 
 
-while (isRunning)
-{
-Console.WriteLine("Choose Rock, Paper or Scissors");
->>>>>>> fdeb87fb1f5db269f4b2a89f7d189a5949b68a33
 
-string userInput = Console.ReadLine();
 
-switch (computer)
-{
-    case: "rock"
-    if (player == "paper")
-    {
-        Console.WriteLine("You Win!!!");
-    }
-    else if (player == "scissors")
-    {
-        Console.WriteLine("You Lose Bitch!!!!");
-    } else if (player == "rock")
-    {
-        Console.WriteLine("It's A Tie!!!");
-    }
-    break;
-
-    case: "paper"
-    if (player == "rock")
-    {
-        Console.WriteLine("You Lose!!!");
-    }
-    else if (player == "Scissors")
-    {
-        Console.WriteLine("You Win!!!");
-    }
-    else if (player == "paper")
-    {
-        Console.WriteLine("It's A Tie!!!");
-    }
-    break;
-
-    case: "scissors"
-    if (player == "rock")
-    {
-        Console.WriteLine("You Win!!!");
-    }
-    else if (player == "Paper")
-    {
-        Console.WriteLine("You Lose!!!");
-    }
-    else if (player == "scissors")
-    {
-        Console.WriteLine("It's a Tie!!!");
-    }
-}
-
-}
--- ConsoleGame/ConsoleGame.cs/Repository.cs
+++ ConsoleGame/ConsoleGame.cs/Repository.cs
@@ -8,17 +8,119 @@ namespace ConsoleGame.cs
     public class Repository
     {
         //wrapping the choises to enter in this while loop powered by a bool will keep our players from being able to enter anything other than Rock, Paper, Scissors
+    public void RockPaperScissors()
+    {
+    // string[] options = {"rock", "paper", "scissors"};
+    // Random optionsIndex = new Random();
+    bool isRunning = true;
+    // int computer = Random.Next(1, 3);
+    string computer = "";
+
      while (isRunning)
      {
-         player = "";
-         computer = "";
-         while(player != "Rock" && player != "Paper" && player != "Scissors") {
-             Console.WriteLine("Please choose either Rock, Paper, or Scissors");
-             player = Console.ReadLine();
-         }
-         Console.WriteLine(player);
+    string player = "";
+    // string answer = "";
 
-         
-     }
+         while(player != "rock" && player != "paper" && player != "scissors")
+          {
+             Console.WriteLine("Please choose either Rock, Paper, or Scissors");
+            player = Console.ReadLine();
+
+            // string computer = options[optionsIndex.Next(options.Length)];
+          }
+        //  Console.WriteLine(player);
+        // Console.WriteLine(computer);
+        
+    switch (computer)
+    {
+        case "rock":
+            computer = "rock";
+            break;
+        case "paper": 
+            computer = "paper";
+            break;
+        case "scissors":
+            computer = "scissors";
+            break;
     }
-}
+    Console.WriteLine("Computer chose " + computer);
+    Console.WriteLine("Player chose " + player);
+    
+     
+
+    
+    switch (computer)
+     
+    {
+    case "rock":
+        if (player == "paper")
+    {
+        Console.Write("Computer played" + computer);
+        Console.WriteLine("You Win!!!");
+    } 
+        else if (player == "scissors")
+    {
+        Console.Write("Computer played" + computer);
+        Console.WriteLine("You Lose Bitch!!!!");
+    } 
+        else if (player == "rock")
+    {
+        Console.Write("Computer played" + computer);
+        Console.WriteLine("It's A Tie!!!");
+    }
+    break;
+
+    case "paper":
+    
+        if (player == "rock")
+    {
+        Console.Write("Computer played" + computer);
+        Console.WriteLine("You Lose!!!");
+    }
+        else if (player == "Scissors")
+    {
+        Console.Write("Computer played" + computer);
+        Console.WriteLine("You Win!!!");
+    }
+        else if (player == "paper")
+    {
+        Console.Write("Computer played" + computer);
+        Console.WriteLine("It's A Tie!!!");
+    }
+    break;
+    
+    
+    
+    case "scissors":
+    
+        if (player == "rock")
+    {
+        Console.Write("Computer played" + computer);
+        Console.WriteLine("You Win!!!");
+    }
+        else if (player == "Paper")
+    {
+        Console.Write("Computer played" + computer);
+        Console.WriteLine("You Lose!!!");
+    }
+        else if (player == "scissors")
+    {
+        Console.Write("Computer played" + computer);
+        Console.WriteLine("It's a Tie!!!");
+    }
+    break;
+                    }
+                }
+            }
+        }
+    }
+
+    
+
+    // default: 
+    // Console.WriteLine("Invalid entry");
+    
+    
+    
+     
+    
